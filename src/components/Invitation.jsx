598036d3ef4f3bb8f1680a54d67f6101dc1244a7
aidancom <19aidan99@gmail.com>
2026-04-26 10:45:54 +0200

    Agregamos sobre

--- src/components/Invitation.jsx
+++ src/components/Invitation.jsx
@@ -1,67 +1,98 @@
 import { useState } from "react"
 import { useInvitationStore } from "../stores/useInvitationStore"
 import "../assets/css/envelope.css"
+import { motion, AnimatePresence } from "framer-motion"
 
 const Invitiation = () => {
 
     const data = useInvitationStore(state => state.data)
 
     const [open, setOpen] = useState(false);
-    const [showCard, setShowCard] = useState(false)
+    const [showCard, setShowCard] = useState(false);
+    const [hideEnvelope, setHideEnvelope] = useState(false);
 
 
     return (
         <>
-            <section className={`cssletter ${showCard ? "hidden" : "flex"}`}>
-            <div className={`envelope ${open ? "active" : ""}`}>
-                <button 
-                    className="heart" 
-                    id="openEnvelope"
-                    onClick={() => {
-                        setOpen(true);
+            <AnimatePresence>
+                {!hideEnvelope && (
+                    <motion.section
+                        className="cssletter flex"
+                        initial={{ opacity: 1 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ 
+                            opacity: 0, 
+                            transition: { duration: 0.6 }
+                        }}
+                        onAnimationComplete={() => {
+                            if (open) {
+                            setTimeout(() => setShowCard(true), 100);
+                            }
+                        }}
+                        >
+                        <div className={`envelope ${open ? "active" : ""}`}>
+                            <button 
+                                className="heart" 
+                                id="openEnvelope"
+                                onClick={() => {
+                                    setOpen(true);
 
-                        setTimeout(() => {
-                            setShowCard(true);
-                        }, 5000);
-                    }}
-                    >
-                <div>
-                    <img src="../img/sello.png" />
-                    <span className="heart-text">Haz click</span>
-                </div>
-                </button>
-                <div className="envelope-flap"></div>
-                <div className="envelope-folds">
-                <div className="envelope-left"></div>
-                <div className="envelope-right"></div>
-                <div className="envelope-bottom"></div>
-                </div>
-            </div>
-            </section>    
-            <div className={`bg-white p-7 z-999 mx-5 ${showCard ? "" : "hidden"}`}>
-                <h1 className="font-[DancingScript] text-[35px] text-center">Hola, {data.guest_information?.guest_name}</h1>
-                <p className="pt-2">
-                    {data.guest_information?.guest_family == "Yes" ? 'Os' : 'Te'} queremos invitar a un evento muy especial y esta es {data.guest_information?.guest_family == "Yes" ? 'vuestra' : 'tu'} invitación
-                </p>
-                {data.guest_information?.guest_family_number ? (
-                    <p className="text-center pt-5 text-xl">
-                        Nº de invitados: {data.guest_information?.guest_family_number}
-                    </p>
-                ) : (
-                    <p>
-                        Nº de invitados: 1
-                    </p>
+                                    setTimeout(() => {
+                                        setHideEnvelope(true);
+                                    }, 2000);
+                                }}
+                                >
+                            <div>
+                                <img src="../img/sello.png" />
+                                <span className="heart-text">Haz click</span>
+                            </div>
+                            </button>
+                            <div className="envelope-flap"></div>
+                            <div className="envelope-folds">
+                            <div className="envelope-left"></div>
+                            <div className="envelope-right"></div>
+                            <div className="envelope-bottom"></div>
+                            </div>
+                        </div>
+                    </motion.section>  
                 )}
-                <div className="flex justify-center items-center mt-5">
-                    <button 
-                        className="bg-[#4C5C37] text-white text-center px-6 py-2 cursor-pointer rounded"
-                        onClick={() => useInvitationStore.setState({seeInvitationPage: true})}
+            </AnimatePresence>  
+            <AnimatePresence>
+                {showCard && (
+                    <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1}}
+                    exit={{ opacity: 0 }}
+                    transition={{ duration: 0.6 }}
+                    className="bg-white p-7 mx-5 z-999"
                     >
-                        Ver Invitación
-                    </button>
-                </div>
+                    <h1 className="font-[DancingScript] text-[35px] text-center">
+                        Hola, {data.guest_information?.guest_name}
+                    </h1>
 
-            </div>                
+                    <p className="pt-2">
+                        {data.guest_information?.guest_family == "Yes" ? 'Os' : 'Te'} queremos invitar a un evento muy especial y esta es {data.guest_information?.guest_family == "Yes" ? 'vuestra' : 'tu'} invitación
+                    </p>
+                    {data.guest_information?.guest_family_number ? (
+                        <p className="text-center pt-5 text-xl">
+                            Nº de invitados: {data.guest_information?.guest_family_number}
+                        </p>
+                    ) : (
+                        <p>
+                            Nº de invitados: 1
+                        </p>
+                    )}
+                    <div className="flex justify-center mt-5">
+                        <button
+                        className="bg-[#4C5C37] text-white px-6 py-2 rounded"
+                        onClick={() => useInvitationStore.setState({seeInvitationPage: true})}
+                        >
+                        Ver Invitación
+                        </button>
+                    </div>
+                    </motion.div>
+                )}
+            </AnimatePresence>            
         </>
 
     )
